c8588ec56afcd47597125dfe2ce02de9340e3d40
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-06-25 03:45:54 -0400

    Add github button

--- src/components/UserOAuth.tsx
+++ src/components/UserOAuth.tsx
@@ -3,29 +3,19 @@ import GithubButton from 'react-github-login-button';
 
 export default function UserOAuth() {
   const { data: session } = useSession();
-  const buttonStyles = {
-    backgroundColor: "#FFC700",
-    color: "#1F2D3D",
-    fontWeight: "bold",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    border: "none",
-    cursor: "pointer",
-    fontFamily: "'Helvetica Neue', sans-serif",
-  };
 
   if (session && session.user) {
     return (
       <>
         Signed in as {session.user.email} <br />
-        <button style={buttonStyles} onClick={() => signOut()}>Sign out</button>
+        <button onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
   return (
     <>
       Not signed in <br />
-      <button style={buttonStyles} onClick={() => signIn()}>Sign in</button>
+      <GithubButton onClick={() => signIn()}>Sign in</GithubButton>
     </>
   );
 }
