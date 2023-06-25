2ea14525ad6b06c63af3d1acc055a2ee56f0f523
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-06-25 05:57:02 -0400

    Add padding

--- src/components/MetamaskButton.tsx
+++ src/components/MetamaskButton.tsx
@@ -12,24 +12,25 @@ const MetamaskButton: React.FC = () => {
       await ethereum.request({ method: 'eth_requestAccounts', params: [] });
       setConnected(true);
     } catch (error) {
-      console.error("Failed to connect to MetaMask", error);
+      console.error('Failed to connect to MetaMask', error);
     }
   };
 
   return (
-    <button 
-      style={{backgroundColor: isConnected ? '#d7c1b3' : '', height: '50px'}}
-      className={`flex items-center justify-center px-6 py-2 rounded-full text-sm font-medium text-gray-700 group-hover:text-gray-900 focus:outline-none ml-4 ${isConnected ? '' : 'space-x-2'}`}
-      onClick={connectWallet}
-    >
-
+    <button
+      style={{ backgroundColor: isConnected ? '#d7c1b3' : '', height: '50px', paddingTop: 25 }}
+      className={`flex items-center justify-center px-6 py-2 rounded-full text-sm font-medium text-gray-700 group-hover:text-gray-900 focus:outline-none ml-4 ${
+        isConnected ? '' : 'space-x-2'
+      }`}
+      onClick={connectWallet}>
       <span className='sr-only'>Open Metamask</span>
-      {isConnected ? 
-        'Connected' :
-        <Image src="/metamask-fox.png" alt="Connect to MetaMask" width={32} height={32} />
-      }
+      {isConnected ? (
+        'Connected'
+      ) : (
+        <Image src='/metamask-fox.png' alt='Connect to MetaMask' width={32} height={32} />
+      )}
     </button>
   );
-}
+};
 
 export default MetamaskButton;
